d394d4514dbe2fd52ecdaab7fe8bb9e977e707a0
Anthony Garza <agarza1972@yahoo.com>
2014-05-12 11:34:28 -0500

    Version 1.1 added counter to display number of guesses

--- highlow.php
+++ highlow.php
@@ -2,7 +2,7 @@
 
 // Game gets a random number
 $rand_number = rand(1,100);
-
+$counter = 1;
 // Write the output
 // Notice the space after the ?
 fwrite(STDOUT, 'Pick a number between 1-100? ');
@@ -15,15 +15,16 @@ while ($guess != $rand_number) {
 	if ($guess > $rand_number) {
 		fwrite(STDOUT, "Lower\n");
 	    $guess = (int) fgets(STDIN);
+	    $counter++;
 	}  
 	else {
 		// If $guess is lower than $rand_number output
         fwrite(STDOUT, "Higher\n");
     	$guess = (int) fgets(STDIN);
+    	$counter++;
     }
 }
 // If $guess is equal to $rand_number output
-fwrite(STDOUT, "#Winner\n");
- 	
+fwrite(STDOUT, "#Winner! It only took {$counter} tries!\n");
 
-?>
+?>	
